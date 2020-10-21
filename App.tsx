0be1f6ea64996ac907198b5733fee3e4584486d3
CatInEars <89234857879@mail.ru>
2020-10-21 14:27:11 +0700

    all app config compleate

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
+/*   
+*                     CatInEars             
+*       Мой GitHub: https://github.com/CatInEars
+*/
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
