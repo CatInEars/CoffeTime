b4db6865d7d3a9259393bfb8f36fbbc216f48b0f
CatInEars <89234857879@mail.ru>
2020-12-16 19:35:51 +0700

    error start fix

--- src/modules/data/coffeShops/coffeShopsDataGenerator.ts
+++ src/modules/data/coffeShops/coffeShopsDataGenerator.ts
@@ -2,15 +2,15 @@ import { ICoffeShop } from "../../../types/data/ICoffeShop";
 import { randomMath } from "../../others/randomMath";
 import { addresArr } from "./arrs/addresArr";
 import { coffeNamesArrFirst, coffeNamesArrSecond } from "./arrs/coffeNamesArr";
-import { imageArr, previewsArr } from "./arrs/imagesArrs";
+import { imagesArr, previewsArr } from "./arrs/imagesArrs";
 
 export function coffeShopsDataGenerator(count: number): ICoffeShop[] {
   const arr = [];
 
   for (let i = 1; i <= count; i++) {
     arr.push({
-      coffePreviewImage: previewsArr[i - 1],
-      coffeOutsideImage: imageArr[i - 1],
+      coffePreviewImage: previewsArr[Math.abs(i-5)],
+      coffeOutsideImage: imagesArr[Math.abs(i-5)],
       name: `${coffeNamesArrFirst[randomMath(0, coffeNamesArrFirst.length - 1)]} ${coffeNamesArrSecond[randomMath(0, coffeNamesArrSecond.length - 1)]}`,
       address: addresArr[randomMath(0, addresArr.length - 1)],
       id: i
